4945dc6519b480ed25e063679396c89e25a2a99f
William Sedlacek <wsedlacekc@gmail.com>
2019-08-26 16:46:43 -0500

    Setup deployment to netlify

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cp ./*.html ./dist/
+cp -R ./css ./dist/
+cp -r ./fonts ./dist/
+cp -r ./scripts ./dist/
+cp ./README.md ./dist/
+git add -A
+git commit -m "Deploy to netlify"
+git push
+git subtree push --prefix dist origin netlify
