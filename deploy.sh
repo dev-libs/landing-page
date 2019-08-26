4d173bf41d712f0fbb529987a5a035127dd1c01f
William Sedlacek <wsedlacekc@gmail.com>
2019-08-26 16:48:53 -0500

    Fix deploymnet

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-cp ./index.html ./dist/
+cp ./*.html ./dist/
 cp -R ./css ./dist/
+cp -r ./fonts ./dist/
+cp -r ./scripts ./dist/
 cp ./README.md ./dist/
+git add -A
+git commit -m "Deploy to master"
+git push
 git subtree push --prefix dist origin master
