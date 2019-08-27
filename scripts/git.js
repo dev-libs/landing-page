aed3aa403edc2461d9948cb1efe0ff1561f25349
William Sedlacek <wsedlacekc@gmail.com>
2019-08-26 23:08:31 -0500

    Update Cache to expire after 2 hours

--- scripts/git.js
+++ scripts/git.js
@@ -5,11 +5,21 @@ class Organization {
 
   async fetchTeams() {
     let cache = localStorage.getItem('organization');
-    const result = cache
-      ? JSON.parse(cache)
-      : await fetch(`https://api.github.com/orgs/${this.name}/public_members`).then(res => res.json());
+    cache = cache ? JSON.parse(cache) : null;
 
-    localStorage.setItem('organization', JSON.stringify(result));
+    const result =
+      cache && cache.expire > Date.now()
+        ? cache.data
+        : await fetch(`https://api.github.com/orgs/${this.name}/public_members`).then(res => res.json());
+
+    if (result.message) {
+      localStorage.removeItem('organization');
+      throw new Error(`Failed to fetch from Github - ${result.message}`);
+    }
+
+    const expire = new Date();
+    expire.setHours(expire.getHours() + 2);
+    localStorage.setItem('organization', JSON.stringify({ expire: expire.getTime(), data: result }));
 
     this.members = result.map(member => {
       return new TeamMember({
