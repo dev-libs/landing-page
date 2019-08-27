3b643cce024ed8025c6f73d08163db20334b31ed
William Sedlacek <wsedlacekc@gmail.com>
2019-08-26 22:42:05 -0500

    Revert "Remove Caching since APIv3 doesn't have a request limit for Org Members"
    This reverts commit c1371947c250d6e253b516bca0907d675f41c5db.

--- scripts/git.js
+++ scripts/git.js
@@ -4,10 +4,15 @@ class Organization {
   }
 
   async fetchTeams() {
-    const result = await fetch(`https://api.github.com/orgs/${this.name}/public_members`).then(res => res.json());
+    let cache = localStorage.getItem('organization');
+    const result = cache
+      ? JSON.parse(cache)
+      : await fetch(`https://api.github.com/orgs/${this.name}/public_members`).then(res => res.json());
+
+    localStorage.setItem('organization', JSON.stringify(result));
 
     this.members = result.map(member => {
-      return new Member({
+      return new TeamMember({
         login: member.login,
         avatar: member.avatar_url,
       });
@@ -15,7 +20,7 @@ class Organization {
   }
 }
 
-class Member {
+class TeamMember {
   constructor(attr) {
     this.login = attr.login;
     this.avatar = attr.avatar;
@@ -40,4 +45,4 @@ class Member {
   }
 }
 
-export { Organization, Member };
+export { Organization, TeamMember };
