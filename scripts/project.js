cdd4a2751fd5ce7c8cf0a316596a1302e2c2de4c
William Sedlacek <wsedlacekc@gmail.com>
2019-08-27 11:07:50 -0500

    Add in Project Information

--- scripts/project.js
+++ scripts/project.js
@@ -1,4 +1,9 @@
 import { Organization } from './git.js';
+import { Technologies } from './technologies.js';
+
+const technologies = [
+  { name: 'react', img: 'https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png' },
+];
 
 const render = async () => {
   const organization = new Organization({ name: 'dev-libs' });
@@ -6,6 +11,16 @@ const render = async () => {
 
   organization.members.forEach(async member => {
     document.querySelector('#members').appendChild(await member.render());
+  });
+
+  const technologies = new Technologies({
+    members: technologies,
+  });
+
+  console.log(technologies);
+
+  technologies.members.forEach(async member => {
+    document.querySelector('#technologies').appendChild(await member.render());
   });
 };
 
