28259e9efb3de4e52e64527fae60a23c03fd8a53
William Sedlacek <wsedlacekc@gmail.com>
2019-08-28 15:59:06 -0500

    Bugs restyle

---
+++ scripts/bugs.js
@@ -0,0 +1,9 @@
+function bugReport() {}
+
+mdc.autoInit();
+
+console.log(mdc);
+
+document.querySelectorAll('.mdc-ripple').forEach(element => mdc.ripple.MDCRipple.attachTo(element));
+document.querySelectorAll('.mdc-text-field').forEach(element => mdc.textField.MDCTextField.attachTo(element));
+document.querySelectorAll('.mdc-select').forEach(element => mdc.select.MDCSelect.attachTo(element));
